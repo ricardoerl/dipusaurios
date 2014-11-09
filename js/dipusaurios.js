a2a33d2a58b1d630ea723706299ccda73e4491c8
Ricardo Ramirez <hola@ricardoerl.com>
2014-11-09 13:45:48 -0600

    Added isotope options

--- js/dipusaurios.js
+++ js/dipusaurios.js
@@ -4,33 +4,37 @@ $(function(){
 			template    = Handlebars.compile(getTemplate);
 
 		var container = $('.content__people').html(template(data)).isotope({
-			itemSelector: '.person',
+			itemSelector: '.card__container',
 			layoutMode: 'fitRows',
 			getSortData : {
-				years: '.years__charge parseInt'
-				},			 
-				sortBy: 'years',
-				sortAscending: false
-			});			
+				years: '.years parseInt'
+			},			 
+			sortBy: 'years',
+			sortAscending: false
+		});			
 
 		$('#by_department').on('change', function() {
-			var filterValue = this.value + ($('#partys button.active').length ? $('#partys button.active').attr('data-filter') : '');
+			var filterValue = this.value + ($("#by_gang option:selected").val());
 		    filterValue = filterValue.replace(/[*]+/, '*');
 
 			container.isotope({
 				filter: filterValue
 			});
 		});
-		$('#partys').on( 'click', 'button', function() {
-		  var filterValue = $(this).attr('data-filter');
-		  $container.isotope({ filter: filterValue });
-		});	
-	});
-	$("#by_department, #by_gang").selectOrDie({
+		$('#by_gang').on('change', function() {
+			var filterValue = this.value + ($("#by_department option:selected").val());
+		    filterValue = filterValue.replace(/[*]+/, '*');
+
+			container.isotope({
+				filter: filterValue
+			});
+		});
+
+		$("#by_department, #by_gang").selectOrDie({
 		cycle: true
-	});
-	$('.card').on( 'click', function() {
-		$( this ).toggleClass( "flip" );
+		});
+		$('.card').on( 'click', function() {
+			$( this ).toggleClass( "flip" );
+		});		
 	});
 });
-
