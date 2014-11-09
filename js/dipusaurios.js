6ac91bd8956bac3a87d5ba7c9a390080b777ff28
Ricardo Ramirez <hola@ricardoerl.com>
2014-11-09 14:20:22 -0600

    Fixing

--- js/dipusaurios.js
+++ js/dipusaurios.js
@@ -4,52 +4,23 @@ $(function(){
 			template    = Handlebars.compile(getTemplate);
 
 		var container = $('.content__people').html(template(data)).isotope({
-<<<<<<< HEAD
-			itemSelector: '.person',
+			itemSelector: '.card__container',
 			layoutMode: 'fitRows',
 			getSortData : {
-				years: '.years__charge parseInt'
+				years: '.years parseInt'
 				},			 
 				sortBy: 'years',
 				sortAscending: false
 			});			
 
 		$('#by_department').on('change', function() {
-			var filterValue = this.value + ($('#partys button.active').length ? $('#partys button.active').attr('data-filter') : '');
-=======
-			itemSelector: '.card__container',
-			layoutMode: 'fitRows',
-			getSortData : {
-				years: '.years parseInt'
-			},			 
-			sortBy: 'years',
-			sortAscending: false
-		});			
-
-		$('#by_department').on('change', function() {
 			var filterValue = this.value + ($("#by_gang option:selected").val());
->>>>>>> gh-pages
 		    filterValue = filterValue.replace(/[*]+/, '*');
 
 			container.isotope({
 				filter: filterValue
 			});
 		});
-<<<<<<< HEAD
-		$('#partys').on( 'click', 'button', function() {
-		  var filterValue = $(this).attr('data-filter');
-		  $container.isotope({ filter: filterValue });
-		});	
-	});
-	$("#by_department, #by_gang").selectOrDie({
-		cycle: true
-	});
-	$('.card').on( 'click', function() {
-		$( this ).toggleClass( "flip" );
-	});
-});
-
-=======
 		$('#by_gang').on('change', function() {
 			var filterValue = this.value + ($("#by_department option:selected").val());
 		    filterValue = filterValue.replace(/[*]+/, '*');
@@ -64,7 +35,7 @@ $(function(){
 		});
 		$('.card').on( 'click', function() {
 			$( this ).toggleClass( "flip" );
-		});		
+		});
+		
 	});
 });
->>>>>>> gh-pages
